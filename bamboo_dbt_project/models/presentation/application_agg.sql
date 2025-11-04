e153cec50927839554c57cb23b72cc78231aa8cc
catrinamitchell12-bit <catrinamitchell12@gmail.com>
2025-11-04 11:56:05 +0000

    fix logic of application_agg

--- bamboo_dbt_project/models/presentation/application_agg.sql
+++ bamboo_dbt_project/models/presentation/application_agg.sql
@@ -7,8 +7,8 @@
 WITH deduplicated_previous_loan as (
 select
     *
-from {{ ref('dim_applications') }}
-qualify row_number() over(partition by application_ref order by event_timestamp asc) = 1
+from ad_hoc.currency.applications
+qualify row_number() over(partition by application_ref, loan_ref order by event_timestamp desc) = 1
 )
 
 SELECT
@@ -20,7 +20,13 @@ SELECT
     application.status,
     application.previous_loan_ref,
     first_value(application.previous_loan_ref) over(partition by application.application_ref order by application.event_timestamp asc) as first_loan_ref_in_series,
+    application.income_check_id,
+    application.income_check,
+    application.mortgage_check_id,
+    application.mortgage_check,
+    application.bills_check_id,
+    application.bills_check,
     application.event_timestamp
 FROM deduplicated_previous_loan application
-JOIN {{ ref('fact_quote') }} quote
+JOIN ad_hoc.currency.quote quote
 ON application.quote_ref = quote.quote_ref
